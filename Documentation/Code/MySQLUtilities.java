33fa3b5ef1745403ae1058e6f930899de5e910fc
Mel Chi <melmadeofsteel@gmail.com>
2015-09-21 17:07:05 -0400

    added comments to the code of MySQLUtilities

--- Documentation/Code/MySQLUtilities.java
+++ Documentation/Code/MySQLUtilities.java
@@ -2,64 +2,123 @@ package cse480;
 
 import java.sql.*;
 
+/**
+ * MySQL Utilites is the basic Select, Update, Insert and Delete
+ * @author Mel Chi and Anthony Quigley 
+ *
+ */
 public class MySQLUtilities{
 	
-	  private static String jdbcDriver = "com.mysql.jdbc.Driver";
-		public String Url = "";
-		public String Username  = "";
-	    public String Password  = "";
+	private static String jdbcDriver = "com.mysql.jdbc.Driver";
+	/**
+	 * Url is the url of where the server is
+	 */
+	public String Url = "";
+	/**
+	 * Username is the username
+	 */
+	public String Username  = "";
+	/**
+	 * Password, password will be stored here when the object is created
+	 */
+    public String Password  = "";
 
-		private Connection theConnection;
+    /**
+     * MySQL connection, global so every function can use it
+     */
+	private Connection theConnection;
 
-		public MySQLUtilities() throws SQLException, ClassNotFoundException
-		{
-	    	Class.forName(jdbcDriver);
-	    	theConnection = DriverManager.getConnection(Url, Username, Password);
-		}
+	/**
+	 * Public constructor
+	 * @throws SQLException
+	 * @throws ClassNotFoundException
+	 */
+	public MySQLUtilities() throws SQLException, ClassNotFoundException
+	{
+    	Class.forName(jdbcDriver);
+    	theConnection = DriverManager.getConnection(Url, Username, Password);
+	}
 
-		public ResultSet SelectSQL(String SelectSQLQuery)throws SQLException
-		{
-		    PreparedStatement thePreparedStatement  = theConnection.prepareStatement(SelectSQLQuery);
-		    return thePreparedStatement.executeQuery();
-		}
+	/**
+	 * 
+	 * @param SelectSQLQuery The Select SQL Statement will go here
+	 * @return Returns a resultSet that way you can get the data
+	 * @throws SQLException
+	 */
+	public ResultSet SelectSQL(String SelectSQLQuery)throws SQLException
+	{
+	    PreparedStatement thePreparedStatement  = theConnection.prepareStatement(SelectSQLQuery);
+	    return thePreparedStatement.executeQuery();
+	}
 
-		public int UpdateSQL(String UpdateSQLStatement)throws SQLException
+	/**
+	 * 
+	 * @param UpdateSQLStatement update sql statement will go here
+	 * @return returns 1 or 0, whether or not it happened
+	 * @throws SQLException
+	 */
+	public int UpdateSQL(String UpdateSQLStatement)throws SQLException
+	{
+		PreparedStatement thePreparedStatement  = theConnection.prepareStatement(UpdateSQLStatement);
+	    return thePreparedStatement.executeUpdate();
+	}
+	
+	/**
+	 * 
+	 * @param DeleteSQLStatement The delete statement
+	 * @return returns 1 or 0
+	 * @throws SQLException
+	 */
+	public int DeleteSQL(String DeleteSQLStatement)throws SQLException
+	{
+		PreparedStatement thePreparedStatement  = theConnection.prepareStatement(DeleteSQLStatement);
+	    return thePreparedStatement.executeUpdate();
+	}
+	
+	/**
+	 * 
+	 * @param InsertSQLStatement Insert SQL Statement
+	 * @return returns 1 or 0 if it is successful or not
+	 * @throws SQLException
+	 */
+	public int InsertSQL(String InsertSQLStatement)throws SQLException
+	{
+		PreparedStatement thePreparedStatement  = theConnection.prepareStatement(InsertSQLStatement, Statement.RETURN_GENERATED_KEYS);
+		thePreparedStatement.executeUpdate();
+		ResultSet theResultSet = thePreparedStatement.getGeneratedKeys();
+		if(theResultSet.next())
 		{
-			PreparedStatement thePreparedStatement  = theConnection.prepareStatement(UpdateSQLStatement);
-		    return thePreparedStatement.executeUpdate();
+			return theResultSet.getInt(1);
 		}
-		
-		public int InsertSQL(String InsertSQLStatement)throws SQLException
+		else
 		{
-			PreparedStatement thePreparedStatement  = theConnection.prepareStatement(InsertSQLStatement, Statement.RETURN_GENERATED_KEYS);
-			thePreparedStatement.executeUpdate();
-			ResultSet theResultSet = thePreparedStatement.getGeneratedKeys();
-			if(theResultSet.next())
-			{
-				return theResultSet.getInt(1);
-			}
-			else
-			{
-				return 0;
-			}
+			return 0;
 		}
-		
-		public void close()
-		{
-			try
-		    {
-				theConnection.close();
-			}
-		    catch (SQLException sqlException)
-		    {
-				sqlException.printStackTrace();
-		    	theConnection = null;
-		    }
+	}
+	
+	/**
+	 * Closes the connection
+	 */
+	public void close()
+	{
+		try
+	    {
+			theConnection.close();
 		}
-		protected void finalize()
-		{
-			close();
-		}
+	    catch (SQLException sqlException)
+	    {
+			sqlException.printStackTrace();
+	    	theConnection = null;
+	    }
+	}
+	
+	/**
+	 * Overrides the finalize object which calls the close function 
+	 */
+	protected void finalize()
+	{
+		close();
+	}
 	
 }
 	
