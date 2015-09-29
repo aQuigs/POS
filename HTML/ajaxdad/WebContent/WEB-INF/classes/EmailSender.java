25b8543a29565ce7d93ad6a63b20ecd0063819b9
aQuigs <aQuigs@users.noreply.github.com>
2015-09-28 21:10:43 -0400

    Get orders for kitchen UI

--- HTML/ajaxdad/WebContent/WEB-INF/classes/EmailSender.java
+++ HTML/ajaxdad/WebContent/WEB-INF/classes/EmailSender.java
@@ -4,53 +4,39 @@ import javax.mail.internet.*;
 
 public class EmailSender
 {
+    private static final String FROM = "aquigley1777@gmail.com";
+    private static final String SMTP_USERNAME = "AKIAILK7KE44MV3ZQDBQ";
+    private static final String SMTP_PASSWORD = "Aou+Dg5o3NZsQvsEXv1vJq1C4EipxvQLcGY6/XfW0/sA";
+    private static final String HOST = "email-smtp.us-east-1.amazonaws.com";
 
-    static final String FROM = "aquigley1777@gmail.com";
-    static final String SMTP_USERNAME = "AKIAILK7KE44MV3ZQDBQ";
-    static final String SMTP_PASSWORD = "Aou+Dg5o3NZsQvsEXv1vJq1C4EipxvQLcGY6/XfW0/sA";
-    static final String HOST = "email-smtp.us-east-1.amazonaws.com";
-
-    // Port we will connect to on the Amazon SES SMTP endpoint. We are choosing port 25 because we
-    // will use
-    // STARTTLS to encrypt the connection.
-    static final int PORT = 25;
+    private static final int PORT = 25;
 
     public static void sendEmail(String to, String subject, String body) throws MessagingException, NoSuchProviderException
     {
-        // Create a Properties object to contain connection configuration information.
         Properties props = System.getProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.port", PORT);
 
-        // Set properties indicating that we want to use STARTTLS to encrypt the connection.
-        // The SMTP session will begin on an unencrypted connection, and then the client
-        // will issue a STARTTLS command to upgrade to an encrypted connection.
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.starttls.required", "true");
 
-        // Create a Session object to represent a mail session with the specified properties.
         Session session = Session.getDefaultInstance(props);
 
-        // Create a message with the specified information.
         MimeMessage msg = new MimeMessage(session);
         msg.setFrom(new InternetAddress(FROM));
         msg.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
         msg.setSubject(subject);
         msg.setContent(body, "text/plain");
 
-        // Create a transport.
         Transport transport = session.getTransport();
 
-        // Send the message.
         try
         {
             System.out.println("Attempting to send an email through the Amazon SES SMTP interface...");
 
-            // Connect to Amazon SES using the SMTP username and password you specified above.
             transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);
 
-            // Send the email.
             transport.sendMessage(msg, msg.getAllRecipients());
             System.out.println("Email sent!");
         }
@@ -61,7 +47,6 @@ public class EmailSender
         }
         finally
         {
-            // Close and terminate the connection.
             transport.close();
         }
     }
---
+++ HTML/ajaxdad/WebContent/WEB-INF/classes/GetUnfinishedOrders.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/GetUnfinishedOrders")
+public class GetUnfinishedOrders extends HttpServlet
+{
+    private static final long serialVersionUID = 1L;
+
+    public GetUnfinishedOrders()
+    {
+        super();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+
+        if (!ServletUtilities.checkSingletonInputs(request, new String[] { "username" }))
+        {
+            writer.append("error");
+            return;
+        }
+
+        try
+        {
+            MySQLUtilities sql = new MySQLUtilities();
+            ResultSet rs = sql.SelectSQL("SELECT OrderDetails.status,MenuDetails.itemName,OrderDetails.miscInfo "
+                    + "FROM UserInfo INNER JOIN OrderList ON UserInfo.restaurantId=OrderList.restaurantId "
+                    + "INNER JOIN OrderDetails ON OrderList.orderId=OrderDetails.orderId "
+                    + "INNER JOIN MenuDetails ON OrderDetails.menuItemId=MenuDetails.menuItemId "
+                    + "WHERE UserInfo.username='" + request.getParameter("username") + "' "
+                    + "AND (OrderDetails.status='placed' OR OrderDetails.status='cooked') "
+                    + "ORDER BY OrderDetails.status;");
+            while (rs.next())
+            {
+                writer.append(rs.getString(1));
+                writer.append(',');
+                writer.append(rs.getString(2));
+                writer.append(',');
+                String miscInfo = rs.getString(3);
+                writer.append(miscInfo == null ? "" : miscInfo);
+                writer.append("<br>");
+            }
+        }
+        catch (ClassNotFoundException e)
+        {
+            writer.append("error");
+        }
+        catch (SQLException e)
+        {
+            writer.append("error");
+        }
+    }
+}
