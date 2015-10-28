bba7b3cde631066825d33330986ac06f260567c0
aQuigs <aQuigs@users.noreply.github.com>
2015-10-28 16:19:46 -0400

    Added Mel's first SQL procedures

---
+++ procedures/AddMenu.sql
@@ -0,0 +1,48 @@
+DROP PROCEDURE AddMenu;
+DELIMITER //
+CREATE PROCEDURE AddMenu(IN IUsername VARCHAR(64), IN IPassword VARCHAR(256), IN IMenuName VARCHAR(256), OUT OReturnCode int(3))
+
+BEGIN
+DECLARE userType varchar(10);
+DECLARE VRestaurantId int(11);
+Select userType, VRestaurantId;
+CALL ValidateUser(IUsername, IPassword, userType);
+
+IF(userType = 'admin')
+    THEN
+    SELECT restaurantId INTO VRestaurantId FROM UserInfo WHERE UserInfo.type = 'admin' AND UserInfo.username = IUsername AND UserInfo.password = IPassword;
+    
+    IF (VRestaurantId > 0)
+        THEN
+        IF EXISTS(SELECT * FROM MenuList WHERE MenuList.restaurantId = VRestaurantId AND MenuList.menuName = IMenuName)
+            THEN
+                SET OReturnCode = -7;
+            ELSE
+                
+                INSERT MenuList (restaurantId,menuName) VALUES (VRestaurantId, IMenuName);
+                IF ROW_COUNT() = -1
+                    THEN
+                        SET OReturnCode = 0;
+                    ELSE
+                        SET OReturnCode = -1;
+                    END IF;
+            END IF;
+        ELSE
+            SET OReturnCode = -2;
+        END IF;
+    
+    ELSEIF (userType = 'kitchen')
+        THEN
+            SET OReturnCode = -3;
+    ELSEIF (userType = 'customer')
+        THEN
+            SET OReturnCode = -4;
+    ELSEIF (userType = 'waitstaff')
+        THEN
+            SET OReturnCode = -5;
+    ELSE
+        SET OReturnCode = -6;
+END IF;
+
+END //
+DELIMITER ;
---
+++ procedures/AddMenuItem.sql
@@ -0,0 +1,41 @@
+DROP PROCEDURE AddMenuItem;
+DELIMITER //
+CREATE PROCEDURE AddMenuItem(IN IUsername VARCHAR(64), IN IPassword VARCHAR(256), IN IMenuId INT(11), IN IItemName VARCHAR(256), IN ICost DECIMAL(6,2), IN ISubMenu VARCHAR(256), IN IDescription VARCHAR(256), OUT OReturnCode int(3))
+
+BEGIN
+DECLARE userType varchar(10);
+DECLARE VRestaurantId int(11);
+
+Select userType, VRestaurantId;
+CALL ValidateUser(IUsername, IPassword, userType);
+
+
+IF(userType = 'admin')
+    THEN
+     IF EXISTS(SELECT MenuList.menuId FROM UserInfo INNER JOIN MenuList ON MenuList.restaurantId=UserInfo.restaurantId AND UserInfo.type='admin' AND UserInfo.username=IUsername AND UserInfo.password=IPassword AND MenuList.menuId=IMenuID)
+        THEN
+        INSERT INTO MenuDetails (menuId,itemName,cost,itemDescription, submenu) VALUES (IMenuId, IItemName, ICost, IDescription, ISubMenu);
+        IF ROW_COUNT() > 0
+            THEN
+                SET OReturnCode = 0;
+            ELSE
+                SET OReturnCode = 8;
+        END IF;
+        ELSE
+            SET OReturnCode = 9;       
+    END IF;
+ELSEIF (userType = 'kitchen')
+    THEN
+        SET OReturnCode = 3;
+ELSEIF (userType = 'customer')
+    THEN
+        SET OReturnCode = 4;
+ELSEIF (userType = 'waitstaff')
+    THEN
+        SET OReturnCode = 5;
+ELSE
+    SET OReturnCode = 6;
+END IF;
+
+END //
+DELIMITER ;
