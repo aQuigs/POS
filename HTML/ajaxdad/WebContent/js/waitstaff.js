fbe15455cb222284c4e462fe333106d568281156
Mitch Moore <mitchmoore@Mitchs-MacBook-Pro.local>
2015-11-12 03:36:56 -0500

    Updated seating functions

--- HTML/ajaxdad/WebContent/js/waitstaff.js
+++ HTML/ajaxdad/WebContent/js/waitstaff.js
@@ -8,16 +8,168 @@ $(document).ready(function () {
     getMenus();
 });
 
-function tableTaken(tableNum)
-{
-	var tableVar = "#table" + tableNum;
-	$(tableVar).toggleClass("taken");
+function ChangeFilledSeats(tableId, filledSeats)
+{ 
+    xmlHttpRequest.open("POST", "ChangeFilledSeats?username=" + getCookie("username").toString() + "&password=" + getCookie("password") + "&tableId=" + tableId.toString() + "&filledSeats=" + filledSeats.toString(), true);
+    xmlHttpRequest.onreadystatechange = fillTable;
+    xmlHttpRequest.send();
 }
 
-function multiTableTaken(tableNum)
+function fillTable()
+{
+    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
+    {
+    	var result = xmlHttpRequest.responseText.split("\n");
+    	console.log(result);
+    }
+}
+
+function tableTaken(tableNum, capacity)
+{
+	var tableVar = "#table" + tableNum;
+	
+	if(!$(tableVar).hasClass("taken"))
+	{
+		var modal = myApp.modal({
+			title: 'Seat Table ' + tableNum,
+			text: 'How many guests will be at this table?',
+			afterText: '<div class="item-inner">' +
+		                   '<div class="item-input">' + 
+		                       '<div class="range-slider">' + 
+		                           '<input id="sitSlider" type="range" min="0" max="' + capacity + '" value="0" step="1">' + 
+		                       '</div></div></div>',
+		    buttons: [
+		    {
+		    	text: 'Cancel'
+		    },
+		    {
+		        text: 'Ok',
+		        bold: true,
+		        onClick: function () {
+		        	var filledSeats = $("#sitSlider").val();
+		        	
+		        	if(filledSeats == 0)
+		        	{
+		        		myApp.alert('No guests were specified, table will not be sat.');
+		        	}
+		        	else
+		        	{
+		        		myApp.showIndicator();
+		        		
+		        		ChangeFilledSeats(tableNum, filledSeats);
+		        		
+		        		myApp.alert('The table has been filled!');
+		        		
+		        		$(tableVar).addClass("taken");
+		        		
+		        		myApp.hideIndicator();
+		        	}
+		        }
+		    },
+		    ]
+		});
+	}
+	else
+	{
+		var modal = myApp.modal({
+			title: 'Empty Table ' + tableNum,
+			text: 'Are you sure you want to clear the table?',
+		    buttons: [
+		    {
+		    	text: 'Cancel'
+		    },
+		    {
+		        text: 'Clear',
+		        bold: true,
+		        onClick: function () {
+		        	myApp.showIndicator();
+		        	
+		        	ChangeFilledSeats(tableNum, 0);
+		        	
+		        	myApp.alert("The table has been cleared!");
+		        	
+		        	$(tableVar).removeClass("taken");
+		        	
+		        	myApp.hideIndicator();
+		        }
+		    },
+		    ]
+		});
+	}
+}
+
+function multiTableTaken(tableNum, capacity)
 {
 	var tableVar = ".table" + tableNum;
-	$(tableVar).toggleClass("taken");
+	
+	if(!$(tableVar).hasClass("taken"))
+	{
+		var modal = myApp.modal({
+			title: 'Seat Table ' + tableNum,
+			text: 'How many guests will be at this table?',
+			afterText: '<div class="item-inner">' +
+		                   '<div class="item-input">' + 
+		                       '<div class="range-slider">' + 
+		                           '<input id="sitSlider" type="range" min="0" max="' + capacity + '" value="0" step="1">' + 
+		                       '</div></div></div>',
+		    buttons: [
+		    {
+		    	text: 'Cancel'
+		    },
+		    {
+		        text: 'Ok',
+		        bold: true,
+		        onClick: function () {
+		        	var filledSeats = $("#sitSlider").val();
+
+		        	if($('#sitSlider').val() == 0)
+		        	{
+		        		myApp.alert('No guests were specified, table will not be sat.');
+		        	}
+		        	else
+		        	{
+		        		myApp.showIndicator();
+		        		
+		        		ChangeFilledSeats(tableNum, filledSeats);
+		        		
+		        		myApp.alert('The table has been filled!');
+		        		
+		        		$(tableVar).addClass("taken");
+		        		
+		        		myApp.hideIndicator();
+		        	}
+		        }
+		    },
+		    ]
+		});
+	}
+	else
+	{
+		var modal = myApp.modal({
+			title: 'Empty Table ' + tableNum,
+			text: 'Are you sure you want to clear the table?',
+		    buttons: [
+		    {
+		    	text: 'Cancel'
+		    },
+		    {
+		        text: 'Clear',
+		        bold: true,
+		        onClick: function () {
+		        	myApp.showIndicator();
+		        	
+		        	ChangeFilledSeats(tableNum, 0);
+		        	
+		        	myApp.alert("The table has been cleared!");
+		        	
+		        	$(tableVar).removeClass("taken");
+		        	
+		        	myApp.hideIndicator();
+		        }
+		    },
+		    ]
+		});
+	}
 }
 
 function addRow(rowNum)
@@ -29,7 +181,7 @@ function addPointDiv(row, column)
 {
 	return "<div class='col-10 " + row + "-" + column + " empty'></div>";
 }
-function addTable(tableId, row, column)
+function addTable(tableId, row, column, capacity)
 {
 	var coordinates = "." + row + "-" + column;
 	var id = "table" + tableId;
@@ -37,10 +189,10 @@ function addTable(tableId, row, column)
 	$(coordinates).addClass("table");
 	$(coordinates).attr('id', id);
 	$(coordinates).removeClass("empty");
-	$(coordinates).attr("onclick", 'tableTaken(' + tableId + ');');
+	$(coordinates).attr("onclick", 'tableTaken(' + tableId + ', ' + capacity + ');');
 }
 
-function addTablePart(tableId, row, column)
+function addTablePart(tableId, row, column, capacity)
 {
 	var coordinates = "." + row + "-" + column;
 	var id = "table" + tableId;
@@ -48,7 +200,7 @@ function addTablePart(tableId, row, column)
 	$(coordinates).addClass("table");
 	$(coordinates).addClass(id);
 	$(coordinates).removeClass("empty");
-	$(coordinates).attr("onclick", 'multiTableTaken(' + tableId + ');');
+	$(coordinates).attr("onclick", 'multiTableTaken(' + tableId + ', ' + capacity + ');');
 }
 
 function createEmptyGrid(gridRows, gridColumns)
@@ -81,6 +233,7 @@ function getLayout()
     	//GridX, GridY then for each table,
     	//tableId, row, column, Width, Height, Capacity, Filled Seats, Booth(YES, NO, Partially)
     	var result = xmlHttpRequest.responseText.split("\n");
+
     	console.log(result);
     	var gridDimensions = result[0].split(",");
     	
@@ -97,14 +250,14 @@ function getLayout()
     			//[3] is WIDTH and [4] is HEIGHT
     			if(currentTable[3] == 1 && currentTable[4] == 1)
         		{
-        			addTable(currentTable[0], currentTable[1], currentTable[2]);
+        			addTable(currentTable[0], currentTable[1], currentTable[2], currentTable[5]);
         		}
         		else if(currentTable[3] != 1 && currentTable[4] == 1)	//Deal with longer tables
         		{
         			//Create tables with width greater than 1
         			for(j = 0; j < currentTable[3]; j++)
         			{
-        				addTablePart(currentTable[0], currentTable[1], (parseInt(currentTable[2]) + j));
+        				addTablePart(currentTable[0], currentTable[1], (parseInt(currentTable[2]) + j), currentTable[5]);
         				
         				var currentColumn = (parseInt(currentTable[2]) + j);
         				var coordinates = "." + currentTable[1] + "-" + currentColumn;
@@ -129,7 +282,7 @@ function getLayout()
         			//Create tables with height greater than 1
         			for(k = 0; k < currentTable[4]; k++)
         			{
-        				addTablePart(currentTable[0], (parseInt(currentTable[1]) + k), currentTable[2]);
+        				addTablePart(currentTable[0], (parseInt(currentTable[1]) + k), currentTable[2], currentTable[5]);
         				
         				var currentRow = (parseInt(currentTable[1]) + k);
         				var coordinates = "." + currentRow + "-" + currentTable[2];
@@ -156,7 +309,7 @@ function getLayout()
         				
         				for(l = 0; l < currentTable[3]; l++)
             			{
-            				addTablePart(currentTable[0], (parseInt(currentTable[1]) + m), (parseInt(currentTable[2]) + l));
+            				addTablePart(currentTable[0], (parseInt(currentTable[1]) + m), (parseInt(currentTable[2]) + l), currentTable[5]);
             				
             				var currentColumn = (parseInt(currentTable[2]) + l);
             				var coordinates = "." + currentRow + "-" + currentColumn;
