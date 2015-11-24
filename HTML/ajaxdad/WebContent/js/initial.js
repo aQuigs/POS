544cd1a4b8000030afce166373357890e2f647b7
Mitch Moore <mitchmoore@Mitchs-MacBook-Pro.local>
2015-11-23 21:06:33 -0500

    updated login

---
+++ HTML/ajaxdad/WebContent/js/initial.js
@@ -0,0 +1,212 @@
+$(function() {
+    
+    var $formLogin = $('#login-form');
+    var $formLost = $('#lost-form');
+    var $formRegister = $('#register-form');
+    var $divForms = $('#div-forms');
+    var $modalAnimateTime = 300;
+    var $msgAnimateTime = 150;
+    var $msgShowTime = 2000;
+
+    $("form").submit(function () {
+        switch(this.id) {
+            case "login-form":
+                var $lg_username=$('#login_username').val();
+                var $lg_password=$('#login_password').val();
+                
+                login($lg_username, $lg_password);
+                
+                return false;
+                break;
+            case "lost-form":
+                var $ls_email=$('#lost_email').val();
+                
+                forgot($ls_email);
+                
+                return false;
+                break;
+            case "register-form":
+                var $rg_username=$('#register_username').val();
+                var $rg_email=$('#register_email').val();
+                var $rg_password=$('#register_password').val();
+                
+                register($rg_username, $rg_email, $rg_password);
+                
+                return false;
+                break;
+            default:
+                return false;
+        }
+        return false;
+    });
+    
+    $('#login_register_btn').click( function () { modalAnimate($formLogin, $formRegister) });
+    $('#register_login_btn').click( function () { modalAnimate($formRegister, $formLogin); });
+    $('#login_lost_btn').click( function () { modalAnimate($formLogin, $formLost); });
+    $('#lost_login_btn').click( function () { modalAnimate($formLost, $formLogin); });
+    $('#lost_register_btn').click( function () { modalAnimate($formLost, $formRegister); });
+    $('#register_lost_btn').click( function () { modalAnimate($formRegister, $formLost); });
+    
+    function modalAnimate ($oldForm, $newForm) {
+        var $oldH = $oldForm.height();
+        var $newH = $newForm.height();
+        $divForms.css("height",$oldH);
+        $oldForm.fadeToggle($modalAnimateTime, function(){
+            $divForms.animate({height: $newH}, $modalAnimateTime, function(){
+                $newForm.fadeToggle($modalAnimateTime);
+            });
+        });
+    }
+    
+    function msgFade ($msgId, $msgText) {
+        $msgId.fadeOut($msgAnimateTime, function() {
+            $(this).text($msgText).fadeIn($msgAnimateTime);
+        });
+    }
+    
+    function msgChange($divTag, $iconTag, $textTag, $divClass, $iconClass, $msgText) {
+        var $msgOld = $divTag.text();
+        msgFade($textTag, $msgText);
+        $divTag.addClass($divClass);
+        $iconTag.removeClass("glyphicon-chevron-right");
+        $iconTag.addClass($iconClass + " " + $divClass);
+        setTimeout(function() {
+            msgFade($textTag, $msgOld);
+            $divTag.removeClass($divClass);
+            $iconTag.addClass("glyphicon-chevron-right");
+            $iconTag.removeClass($iconClass + " " + $divClass);
+  		}, $msgShowTime);
+    }
+    
+  //AJAX Functions for login modal
+    function login(username, password)
+    {
+        xmlHttpRequest.open("POST", "LoginVerification?username=" + username + "&password=" + password, true);
+        xmlHttpRequest.onreadystatechange = processLogin;
+        xmlHttpRequest.send();
+    }
+
+    function processLogin()
+    {
+        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
+        {
+            var results = xmlHttpRequest.responseText.split(',');
+            if (results.length == 2)
+            {
+            	console.log(results);
+                var accountType = results[0];
+                var location = "";
+                
+                if(accountType == "admin")
+                {
+                	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Welcome Admin User");
+                    location = "/POS/admin.html";
+                }
+                else if(accountType == "kitchen")
+                {
+                	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Welcome Kitchen Member!");
+                    location = "/POS/kitchen.html";
+                }
+                else if(accountType == "waitstaff")
+                {
+                	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Welcome Wait Staff Member!");
+                    location = "/POS/waitstaff.html";
+                }
+                else if(accountType == "customer")
+                {
+                	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "success", "glyphicon-ok", "Welcome!");
+                    location = "/POS/index.html";
+                }
+                else
+                {
+                	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Invalid Credentials");
+                }
+                
+                var tempUser = $('#login_username').val();
+
+                setCookie("username", tempUser, 1);
+                setCookie("password", results[1], 1);
+                setCookie("accountType", accountType);
+                window.location.replace(location);
+            }
+            else if(xmlHttpRequest.responseText == "invalid")
+            {
+            	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Invalid Credentials");
+            }
+            else if (xmlHttpRequest.responseText == "unverified")
+            {
+                msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "Please activate your account first; see your email for the activation link");
+            }
+            else if (xmlHttpRequest.responseText == "error")
+            {
+            	msgChange($('#div-login-msg'), $('#icon-login-msg'), $('#text-login-msg'), "error", "glyphicon-remove", "An error has occurred. Please try again later");
+            }
+        }
+    }
+    
+    function register(username, password, email)
+    {
+        xmlHttpRequest.open("POST", "Registration?username=" + username + "&password=" + password + "&email=" + email, true);
+        xmlHttpRequest.onreadystatechange = registerAccount;
+        xmlHttpRequest.send();
+    }
+
+    function registerAccount()
+    {
+        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
+        {
+            var result = xmlHttpRequest.responseText;
+            
+            console.log(result)
+            
+            if (result == "error" || result == "failed")
+            {
+            	msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", "Register error");
+            }
+            else if (result == "success")
+            {
+            	msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "success", "glyphicon-ok", "Registered Successfully! An email will be sent momentarily");
+            }
+            else
+            {
+            	msgChange($('#div-register-msg'), $('#icon-register-msg'), $('#text-register-msg'), "error", "glyphicon-remove", result);
+            }
+        }
+    }
+    
+    function forgot(email)
+    {
+        xmlHttpRequest.open("POST", "ResetPassword?email=" + email, true);
+        xmlHttpRequest.onreadystatechange = forgotPassword;
+        xmlHttpRequest.send();
+    }
+
+    function forgotPassword()
+    {
+        if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
+        {
+            var result = xmlHttpRequest.responseText;
+            
+            console.log(result);
+            
+            if (result == "invalid")
+            {
+            	msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "Invalid Email");
+            }
+            else if (result == "error")
+            {
+            	msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", "An error has occurred");
+            }
+            else if (result == "success")
+            {
+            	msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "success", "glyphicon-ok", "An email will be sent shortly with your new password");
+                //window.location.replace("/POS/login.html");
+            }
+            else
+            {
+            	msgChange($('#div-lost-msg'), $('#icon-lost-msg'), $('#text-lost-msg'), "error", "glyphicon-remove", result);
+            }
+        }
+    }
+});
+
--- HTML/ajaxdad/WebContent/js/my-app.js
+++ HTML/ajaxdad/WebContent/js/my-app.js
@@ -28,63 +28,6 @@ function enterLogin(event)
     }
 }
 
-function login()
-{
-    xmlHttpRequest.open("POST", "LoginVerification?username=" + document.getElementById('username').value + "&password=" + document.getElementById('password').value, true);
-    xmlHttpRequest.onreadystatechange = processLogin;
-    xmlHttpRequest.send();
-}
-
-function processLogin()
-{
-    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
-    {
-        var results = xmlHttpRequest.responseText.split(',');
-        if (results.length == 2)
-        {
-            var accountType = results[0];
-            var location = "";
-            
-            if(accountType == "admin")
-            {
-                location = "/POS/admin.html";
-            }
-            else if(accountType == "kitchen")
-            {
-                location = "/POS/kitchen.html";
-            }
-            else if(accountType == "waitstaff")
-            {
-                location = "/POS/waitstaff.html";
-            }
-            else if(accountType == "customer")
-            {
-                location = "/POS/";
-            }
-            else
-            {
-            	myApp.alert("Unable to login with those credentials");
-            }
-
-            setCookie("username", document.getElementById('username').value, 1);
-            setCookie("password", results[1], 1);
-            window.location.replace(location);
-        }
-        else if(xmlHttpRequest.responseText == "invalid")
-        {
-            alert('Invalid username/password combination');
-        }
-        else if (xmlHttpRequest.responseText == "unverified")
-        {
-            alert('Please activate your account first; see your email for the activation link');
-        }
-        else if (xmlHttpRequest.responseText == "error")
-        {
-            alert('An error occurred');
-        }
-    }
-}
-
 function forgotRedirect()
 {
     window.location.replace("/POS/forgot.html");
@@ -93,61 +36,6 @@ function forgotRedirect()
 function registerRedirect()
 {
     window.location.replace("/POS/register.html");
-}
-
-function forgot()
-{
-    xmlHttpRequest.open("POST", "ResetPassword?email=" + document.getElementById('email').value, true);
-    xmlHttpRequest.onreadystatechange = processReset;
-    xmlHttpRequest.send();
-}
-
-function processReset()
-{
-    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
-    {
-        var result = xmlHttpRequest.responseText;
-        if (result == "invalid")
-        {
-            alert("invalid email");
-        }
-        else if (result == "error")
-        {
-            alert("an error occurred");
-        }
-        else if (result == "success")
-        {
-            alert("An email with your new password has been sent to your account");
-            window.location.replace("/POS/login.html");
-        }
-    }
-}
-function register()
-{
-    xmlHttpRequest.open("POST", "Registration?username=" + document.getElementById('username').value + "&password=" + document.getElementById('password').value + "&email=" + document.getElementById('email').value, true);
-    xmlHttpRequest.onreadystatechange = processReset;
-    xmlHttpRequest.send();
-}
-
-function processReset()
-{
-    if(xmlHttpRequest.readyState == 4 && xmlHttpRequest.status == 200)
-    {
-        var result = xmlHttpRequest.responseText;
-        if (result == "taken")
-        {
-            alert("Username/email already in use");
-        }
-        else if (result == "error" || result == "failure")
-        {
-            alert("An error occurred");
-        }
-        else if (result == "success")
-        {
-            alert("Your account has been created and a verification email has been sent to your email, please access the link inside to activate your account");
-            window.location.replace("/POS/login.html");
-        }
-    }
 }
 
 //Kitchen Interface Swipe Notifications
