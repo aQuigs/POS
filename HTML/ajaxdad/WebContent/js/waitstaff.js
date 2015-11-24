e4dc410657baeddc114a83e111b7aa3b26750bf4
aQuigs <aQuigs@users.noreply.github.com>
2015-11-23 23:34:17 -0500

    Abstracting admin table layout

--- HTML/ajaxdad/WebContent/js/waitstaff.js
+++ HTML/ajaxdad/WebContent/js/waitstaff.js
@@ -8,7 +8,7 @@ function ChangeFilledSeats(tableId, filledSeats, callback)
 	performPost("ChangeFilledSeats?username=" + getCookie("username").toString() + "&password=" + getCookie("password") + "&tableId=" + tableId.toString() + "&filledSeats=" + filledSeats.toString(), callback);
 }
 
-function tableTaken(tableNum, capacity)
+function tableClicked(tableNum, capacity)
 {
     var tableVar = "#table-" + tableNum;
     
@@ -92,79 +92,7 @@ function showVal(value)
     document.getElementById("sliderVal").innerHTML = value;
 }
 
-function getTableLayout()
-{ 
-	performPost("GetTableLayout?username=" + getCookie("username").toString() + "&password=" + getCookie("password"), getLayout);
-}
-
-var gridDimensions;
-
-function handleResize()
-{
-    if (gridDimensions)
-    {       
-        var parent = $('#seating-grid');
-        var hcol = parent.height() / (parseInt(gridDimensions[0]));
-        var wcol = parent.width()  / (parseInt(gridDimensions[1]));
-        var padding = wcol * 0.1;
-
-        parent.children('div').each(function () {
-            $(this).width($(this).attr('w') *wcol-padding);
-            $(this).height($(this).attr('h')*hcol-padding);
-
-            $(this).css({left: ($(this).attr('x')-1)*wcol + "px", top: ($(this).attr('y')-1)*hcol + "px", position: 'absolute'});
-        });
-    }
-}
-
-function getLayout(responseText)
-{
-    var lastResult = responseText.split(";;");
-
-    gridDimensions = lastResult[0].split("::");
-    
-    var parent = $('#seating-grid');
-    parent.empty();
-
-    for (var i = 1; i < lastResult.length; ++i)
-    {
-        if (lastResult[i].length)
-        {
-            var attribs = lastResult[i].split("::");
-            var tableId = parseInt(attribs[0]);
-            var x = parseInt(attribs[1]);
-            var y = parseInt(attribs[2]);
-            var w = parseInt(attribs[3]);
-            var h = parseInt(attribs[4]);
-            var capacity = parseInt(attribs[5]);
-            var filledSeats = parseInt(attribs[6]);
-            var booth = attribs[7];
-            parent.append("<div class='table' id='table-" + tableId + "' onclick='tableTaken("+tableId+","+capacity+");'></div>");
-
-            var d = $('#table-' + tableId)
-
-            d.attr('x',x);
-            d.attr('y',y);
-            d.attr('w',w);
-            d.attr('h',h);
-            d.attr('capacity',capacity);
-            d.attr('filledSeats',filledSeats);
-            d.attr('booth',booth);
-            if (filledSeats !== 0) {
-                d.addClass('taken');
-            }
-        }
-    }
-    handleResize();
-}
-
 $(document).ready(function () {
     restaurantId=1; // TODO get this from server based on credentials
     getMenus();
-
-	$(window).resize(handleResize);
-
-	myApp.onPageInit('seating', function (page) {
-	    getTableLayout();
-	});
 });
